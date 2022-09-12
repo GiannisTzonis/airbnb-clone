486fd15edbc1ab69dd9219bb2a936f0c99af168e
Giannis Tzonis <tzonis.john@hotmail.com>
2022-09-12 03:40:15 +0300

    fixed bugs - added styles - working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-// import Card from "./components/Card"
+import Card from "./components/Card"
 import data from "./data"
+/*
+Challenge:
 
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
 
 export default function App() {
-
+            // <Hero />
     const cards = data.map(item => {
         return (
             <Card 
@@ -18,8 +28,8 @@ export default function App() {
                 price={item.price}
             />
         )
-})
-
+    })        
+    
     return (
         <div>
             <Navbar />
