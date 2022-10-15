eae7f819a8a51c04f1a272178ef2a5c9635f909a
Giannis Tzonis <tzonis.john@hotmail.com>
2022-10-15 07:38:02 +0300

    basic set-up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+
+
+export default function App() {
+    return (
+        <hi>Hi</hi>
+    )
+}
