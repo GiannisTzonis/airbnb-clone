38a06aeaf30a487c3ed657b920e5e9d4f9c0027a
Giannis Tzonis <tzonis.john@hotmail.com>
2022-10-18 06:52:25 +0300

    final polish

--- src/Joke.js
+++ src/Joke.js
@@ -4,7 +4,7 @@ export default function Joke(props) {
     console.log(props)
     return (
         <div>
-            <h3>{props.setup}</h3>
+            {props.setup && <h3>{props.setup}</h3>}
             <p>{props.punchline}</p>
             <hr />
         </div>
